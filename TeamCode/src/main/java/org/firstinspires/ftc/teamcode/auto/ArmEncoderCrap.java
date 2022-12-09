38e57072d94463c448367ad2faff9ac0a41176d9
TheCometH <56562284+TheCometH@users.noreply.github.com>
2022-12-08 17:37:30 -0800

    Update ArmEncoderCrap.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ArmEncoderCrap.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ArmEncoderCrap.java
@@ -35,28 +35,150 @@ public class ArmEncoderCrap extends LinearOpMode {
     Servo tilt;
     Servo clamp;
 
+    static double arm_ticks_per_rev = 5281.1;
+    static double expand_ticks_per_rev = 384.5;
+    static double ticks_per_angle = arm_ticks_per_rev/360;
+    static int ticks;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        // trajectory to tall pole (theoretically)
-        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-        drive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
-        Trajectory traj1 = drive.trajectoryBuilder(new Pose2d(12, 46.6666666666))
-                .splineTo(new Vector2d(12, 72), 0)
-                .addDisplacementMarker( () -> {
-                    // estimations on 90 deg to roate it to the right height
-                    drive.rotate(0.5, 360);
-                    // change the distance to however much the expansion needs
-                    drive.expand(0.1, 2880);
+        rotation = hardwareMap.get(DcMotorEx.class, "rotation");
+        expansion = hardwareMap.get(DcMotorEx.class, "expansion");
+        traverse = hardwareMap.get(DcMotorEx.class, "traverse");
+        tilt = hardwareMap.get(Servo.class, "tilt");
 
-                    drive.stopArmMovement();
-                })
-                .splineTo(new Vector2d(48, 72), Math.toRadians(90))
-                .build();
+        rotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        expansion.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        traverse.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
+        rotation.setDirection(DcMotorSimple.Direction.FORWARD);
+        expansion.setDirection(DcMotorSimple.Direction.FORWARD);
+        traverse.setDirection(DcMotorSimple.Direction.FORWARD);
+        tilt.setDirection(Servo.Direction.FORWARD);
+        clamp.setDirection(Servo.Direction.FORWARD);
+
+        tilt.setPosition(0.705);
+        clamp.setPosition(0);
+        rotation.setPower(0);
+        expansion.setPower(0);
+        traverse.setPower(0);
 
         waitForStart();
-        //if(isStopRequested()) return;
-        //drive.followTrajectory(traj1);
+
+        int rotationInitialPosition = rotation.getCurrentPosition();
+        telemetry.addData("Rotation Initial Position: ", rotationInitialPosition);
+        telemetry.update();
+        rotate(0.1, (int) ((arm_ticks_per_rev/2) + rotationInitialPosition));
+
+        int rotationHalfRevolutionPosition = rotation.getCurrentPosition();
+        telemetry.addData("Rotation Half Revolution Position: ", rotationHalfRevolutionPosition);
+        telemetry.update();
+        sleep(2000);
+
+        rotate(0.1, rotationInitialPosition);
+        sleep(3000);
+
+
+
+        int traverseInitialPosition = traverse.getCurrentPosition();
+        telemetry.addData("Traverse Initial Position: ", traverseInitialPosition);
+        telemetry.update();
+        traversing(0.1, (int) ((arm_ticks_per_rev/2) + traverseInitialPosition));
+
+        int traverseHalfRevolutionPosition = traverse.getCurrentPosition();
+        telemetry.addData("Traverse Half Revolution Position: ", traverseHalfRevolutionPosition);
+        telemetry.update();
+        sleep(2000);
+
+        traversing(0.1, traverseInitialPosition);
+        sleep(3000);
+
+
+
+        int expansionInitialPosition = expansion.getCurrentPosition();
+        telemetry.addData("Traverse Initial Position: ", expansionInitialPosition);
+        telemetry.update();
+        rotate(0.1, (int) ((expand_ticks_per_rev/2) + expansionInitialPosition));
+
+        int expansionHalfRevolutionPosition = expansion.getCurrentPosition();
+        telemetry.addData("Traverse Half Revolution Position: ", expansionHalfRevolutionPosition);
+        telemetry.update();
+        sleep(2000);
+
+        expand(0.1, expansionInitialPosition);
+        sleep(3000);
+
+        openClaw();
+        closeClaw();
+    }
+
+    public void stopArmMovement() {
+        expansion.setPower(0);
+        rotation.setPower(0);
+        traverse.setPower(0);
+    }
+
+    public void rotate(double power, int distance) {
+        rotation.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        rotation.setTargetPosition(distance);
+
+        rotation.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        rotation.setPower(power);
+
+        while (rotation.isBusy()) {
+
+        }
+
+        stopArmMovement();
+        rotation.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void expand(double power, int distance) {
+        expansion.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        expansion.setTargetPosition(distance);
+
+        expansion.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        expansion.setPower(power);
+
+        while (expansion.isBusy()) {
+
+        }
+
+        stopArmMovement();
+        expansion.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public void traversing(double power, int distance) {
+        traverse.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        traverse.setTargetPosition(distance);
+
+        traverse.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        traverse.setPower(power);
+        while (traverse.isBusy()) {
+
+        }
+
+        stopArmMovement();
+        traverse.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+    public int distance(double angle) {
+        ticks = (int) (angle * ticks_per_angle);
+        return ticks;
+    }
+
+    public void closeClaw() {
+        clamp.setPosition(0.3);
+    }
+
+    public void openClaw() {
+        clamp.setPosition(0.6);
     }
 }
